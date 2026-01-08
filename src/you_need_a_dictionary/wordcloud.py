d82fff9f337b0e1bb49d19c5595018e91db51a07
mailysg8 <mai.guedon@gmail.com>
2026-01-07 21:09:51 -0800

    Removed helper function and finished editing docstrings

--- src/you_need_a_dictionary/wordcloud.py
+++ src/you_need_a_dictionary/wordcloud.py
@@ -1,56 +1,39 @@
 """
-A module that creates wordclouds of antanonyms and/or synonyms of a given word .
-
-This module contains functions to:
-- create the wordclouds
-- find 
+A module that creates wordclouds of antonyms and/or synonyms for a given word.
 
 """
-
 def create_wordcloud(word, type='both'):
     """
-    This function creates a wordcloud of either antonyms, synonyms or both for a given word.
+    This function creates a wordcloud of either antonyms, 
+    synonyms or both for a given word.
 
+    The word in the middle of the wordcloud is the word 
+    we want the antonyms/synonyms for.
+    The size of the words in the wordcloud are determined 
+    by the strength of the similarity between the given word and its antonym/synonym. 
+    The similarity score can be found using the NLTK wordnet 
+    package and ranges between 0 and 1 with 1 meaning strong similarity and 0 means weak similarity.
 
     Parameters
     ----------
     word : string
-        The word that we want the synonyms and antonyms.
+        The central word in the wordcloud for which we 
+        want to find the synonyms/antonyms.
     type : string
+        This determines whether the wordcloud contains antonyms or synonyms or whether 
+        the function outputs two wordclouds (one for antonyms and one for synonyms).
         Either 'antonym', 'synonym', or 'both'. Default is 'both'
 
     Returns
     -------
-    float
-        A wordcloud containing antonyms and/or synonyms for a given word.
+    plt.Figure
+        A Matplotlib figure of a wordcloud containing antonyms and/or synonyms for a given word.
 
     Examples
     --------
-    
-    """
-    ...
+    >>> create_wordcloud('car', 'antonym')
 
-def find_strength(word, other_word):
-    """
-    This function finds the strength of the similarity between 2 words.
-
-    Parameters
-    ----------
-    word : string
-        The word that we want the synonyms and antonyms.
-    other_word : string
-        The antonym or synonym that we want to compare.
-
-    Returns
-    -------
-    float
-        The strength of the similarity of the 2 words. It ranges between 0 and 1 where 1 means 
-        strong similarity between words and 0 means weak similarity.
-
-    Examples
-    --------
-    
-
+    >>> create_wordcloud('happy')
     
     """
     ...
