219977962ca5f378f04a282220612c74c3b05652
mailysg8 <mai.guedon@gmail.com>
2026-01-13 17:29:00 -0800

    Added first test

--- tests/unit/test_create_wordcloud.py
+++ tests/unit/test_create_wordcloud.py
@@ -1,5 +1,13 @@
 """
 A test module that tests the wordcloud function.
 """
+import matplotlib.pyplot as plt
+from src.you_need_a_dictionary.wordcloud import create_wordcloud
+import pytest
+
 def test_create_wordcloud():
-    ...
+
+    results = create_wordcloud('door')
+
+    # Verify return type 
+    assert isinstance(results, plt.Figure)
