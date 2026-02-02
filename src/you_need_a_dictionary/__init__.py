d42eaf09e23e23527001f9b813b6c31d972e81d2
mailysg8 <mai.guedon@gmail.com>
2026-02-02 12:30:53 -0800

    Added __version__ to init file

--- src/you_need_a_dictionary/__init__.py
+++ src/you_need_a_dictionary/__init__.py
@@ -8,5 +8,6 @@ from you_need_a_dictionary.sentiment_analysis import analyze_sentiment
 from you_need_a_dictionary.translate_sentence import translate_sentence
 from you_need_a_dictionary.word_replacement import word_replacement
 from you_need_a_dictionary.wordcloud import create_wordcloud
+from .__version__ import __version__
 
-__all__ = ['word_replacement','create_wordcloud','fetch_definition','analyze_sentiment','translate_sentence']
+__all__ = ['word_replacement','create_wordcloud','fetch_definition','analyze_sentiment','translate_sentence','__version__']
